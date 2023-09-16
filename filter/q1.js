81f749dd00d1025080508d69106b3130bf931fc5
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-09-16 16:48:50 +0530

    filter method

---
+++ filter/q1.js
@@ -0,0 +1,3 @@
+const numbers = [1,2,3,4];
+const even = numbers.filter(item => item%2 === 0);
+console.log(even)
---
+++ filter/q2.js
@@ -0,0 +1,12 @@
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+];
+
+const studentsGrades = students.filter(
+    student => student.grade >= 90
+)
+console.log(studentsGrades)
